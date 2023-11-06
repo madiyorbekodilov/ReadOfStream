0c4b051ee9fdc1897030122ac3d7b5b6b6ba24a8
Ferganskiy <odilovmadiyor029@gmail.com>
2023-11-06 13:32:50 +0500

    additions have been added

--- ReadOfStream/Program.cs
+++ ReadOfStream/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using ReadOfStream.DbContexts;
+using ReadOfStream.Interface;
+using ReadOfStream.Services;
+
+var serviceProvider = ConfigureServices();
+
+using (var scope = serviceProvider.CreateScope())
+{
+    var userService = scope.ServiceProvider.GetRequiredService<IUserService>();
+
+    // Your application logic here
+    // Example:
+    var userId = Guid.NewGuid();
+    var user = userService.GetUserById(userId);
+    Console.WriteLine($"User ID: {userId}, User Name: {user?.Name}");
+
+    // Don't forget to dispose of the services when done
+}
+
+static IServiceProvider ConfigureServices()
+{
+    var services = new ServiceCollection();
+
+    services.AddDbContext<AppDbContext>();
+    services.AddScoped<IUserService, UserService>();
+
+    return services.BuildServiceProvider();
+}
