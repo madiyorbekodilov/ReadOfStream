8a8ee5476ec14ef9129b8fd52cef600ca1acf6ca
Ferganskiy <odilovmadiyor029@gmail.com>
2023-11-06 11:06:08 +0500

    AppDbContext added and fixed bugs

---
+++ ReadOfStream/DbContexts/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReadOfStream.Models;
+
+namespace ReadOfStream.DbContexts;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<TagToUser> TagToUsers { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlServer("YourConnectionStringHere");
+    }
+}
