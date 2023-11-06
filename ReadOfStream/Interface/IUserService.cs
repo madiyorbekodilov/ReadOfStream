13506b9c10be66ae9caece26822d83d2784d73a0
Ferganskiy <odilovmadiyor029@gmail.com>
2023-11-06 11:39:33 +0500

    User Service's Iinterface added

---
+++ ReadOfStream/Interface/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using ReadOfStream.Models;
+
+namespace ReadOfStream.Interface;
+
+public interface IUserService
+{
+    User GetUserById(Guid userId);
+    IEnumerable<User> GetUsersByDomain(string domain, int page, int pageSize);
+    IEnumerable<User> GetUsersByTagAndDomain(string tagValue, string domain);
+}
